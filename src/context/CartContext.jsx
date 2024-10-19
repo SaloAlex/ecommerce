c1b5a16b7a78cd4a69963ca24a0dabaa33939ed9
SaloAlex <dvdsalomon6@gmail.com>
2024-10-19 15:14:33 -0300

    correccion cartcontext

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,12 +1,10 @@
-import { createContext, useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-
-export const CartContext = createContext();
+import { CartContext } from './CartContext';  // importas el contexto
 
 export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState(() => {
     try {
-      // Intenta cargar los productos del carrito desde localStorage
       const storedCart = localStorage.getItem('cartItems');
       return storedCart ? JSON.parse(storedCart) : [];
     } catch (error) {
@@ -17,36 +15,35 @@ export const CartProvider = ({ children }) => {
 
   useEffect(() => {
     try {
-      // Guarda el carrito en localStorage cada vez que cambia
       localStorage.setItem('cartItems', JSON.stringify(cartItems));
     } catch (error) {
       console.error("Error al guardar el carrito en localStorage:", error);
     }
   }, [cartItems]);
 
-  // Función para agregar un producto al carrito
   const addToCart = (product) => {
     setCartItems((prevItems) => {
       const existingProduct = prevItems.find((item) => item.id === product.id);
-  
       if (existingProduct) {
-        // Si el producto ya está en el carrito, incrementamos su cantidad
+        const totalQuantity = existingProduct.quantity + product.quantity;
+        if (totalQuantity > product.stock) {
+          console.warn(`Cantidad excede el stock disponible (${product.stock} unidades disponibles)`);
+          return prevItems;
+        }
         return prevItems.map((item) =>
-          item.id === product.id 
-            ? { ...item, quantity: item.quantity + product.quantity } // Aumentar la cantidad basada en lo seleccionado
+          item.id === product.id
+            ? { ...item, quantity: totalQuantity }
             : item
         );
       } else {
-        // Si no está en el carrito, lo agregamos con la cantidad seleccionada
         return [...prevItems, { ...product, quantity: product.quantity }];
       }
     });
   };
 
-  // Función para vaciar el carrito
   const clearCart = () => {
-    setCartItems([]); // Limpia el estado
-    localStorage.removeItem('cartItems'); // Elimina del localStorage
+    setCartItems([]);
+    localStorage.removeItem('cartItems');
   };
 
   return (
@@ -56,9 +53,6 @@ export const CartProvider = ({ children }) => {
   );
 };
 
-// Validación de PropTypes para el CartProvider
 CartProvider.propTypes = {
   children: PropTypes.node.isRequired,
 };
-
-export default CartProvider;
